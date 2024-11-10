a7f9c726706b60797099eadb8355e812435f56e2
Ivan <musatov.i.a@edu.mirea.ru>
2024-11-10 16:34:34 +0300

    feat(visualizer): created script that displays graph code on the auxiliary window

---
+++ Homeworks/hw_2/visualizer.py
@@ -0,0 +1,30 @@
+import argparse
+import tkinter as tk
+from tkinter import scrolledtext as st
+
+# Displaying a window with graph code text
+def display(text: str)->None:
+    window = tk.Tk()
+    window.title("Graph code")
+    window.wm_attributes("-topmost", 1)
+    window.resizable(False, True)
+    window.grid_rowconfigure(0, weight=0)
+    window.grid_columnconfigure(0, weight=1)
+    display = st.ScrolledText(window, height=40, width=100)
+    display.configure(background="#000000", foreground="#FFFFFF")
+    display.grid(row=0, column=0)
+    display.insert(tk.END, f"{text}")
+    display.config(state='disabled')
+    window.mainloop()
+
+def main()->None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("src_path", help="Path to graph code")              # branch name
+    src_path = parser.parse_args().src_path
+    src_path = "res.mermaid"
+    with open(src_path, "r") as src:
+        text = src.read()
+        display(text)
+
+if __name__ == "__main__":
+    main()
