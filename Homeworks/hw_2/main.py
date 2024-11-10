57406c48f85fac73ef10b732e4a69b1dfa2ef0f3
Ivan <musatov.i.a@edu.mirea.ru>
2024-11-10 18:50:50 +0300

    fix(main script): deleted redunant note checking in dictionary

--- Homeworks/hw_2/main.py
+++ Homeworks/hw_2/main.py
@@ -17,10 +17,10 @@ def get_args()->list:
 
 # Choosing branch and store information about commits
 def get_commits_info(repo_path: str, branch_name: str)->dict: 
-    if ".git" in os.listdir(repo_path):
-        repo_path = os.path.join(repo_path, ".git")
     try:
-        branches_path = os.path.join(repo_path, "refs", "heads")    # Path to branches references
+        if ".git" in os.listdir(repo_path):                                 # Handling the absence of .git folder in imitative git-repo
+            repo_path = os.path.join(repo_path, ".git")
+        branches_path = os.path.join(repo_path, "refs", "heads")            # Path to branches references
         if not branch_name in os.listdir(branches_path):                    # Handling absence of that branch
             print(f"{'\033[91m'}There is no branch with name '{branch_name}' in this git tree!{'\033[0m'}")
             with open(os.path.join(repo_path, "HEAD"), "r") as href:
@@ -29,11 +29,11 @@ def get_commits_info(repo_path: str, branch_name: str)->dict:
                     branch_name = content[5:].split('/')[-1].strip()        # Choosing current branch instead of non-existent
                 else:
                     print(f"{'\033[91m'}Well, there is no git branches in this repo at all!{'\033[0m'}")
-                    sys.exit(0)
+                    # sys.exit(0)
             print(f"{'\033[91m'}So, the commits graph will be built for the current branch '{branch_name}'{'\033[0m'}")
         
         dict_info = {}                                                      # key - commit hash, value = list of [[parents], date, author]
-        objects_path = os.path.join(repo_path, "objects")           # Path to git objects
+        objects_path = os.path.join(repo_path, "objects")                   # Path to git objects
         with open(os.path.join(branches_path, branch_name), "r") as bref:
             last_commit_hash = bref.read().strip()                          # Getting hash-reference to last commit in this branch
         commits_bypassing(objects_path, last_commit_hash, dict_info)
@@ -41,7 +41,7 @@ def get_commits_info(repo_path: str, branch_name: str)->dict:
     except FileNotFoundError:                                               # Handling wrong file pathes
         print(f"{'\033[91m'}It seems that you've entered wrong path to repo.\n\
               \rPlease restart program with correct arguments!{'\033[0m'}")
-        sys.exit(-1)
+        # sys.exit(-1)
 
 # Recursive bypassing of commits
 def commits_bypassing(objects_path: str, commit_hash: str, dict_info: dict)->None:
@@ -63,11 +63,10 @@ def commits_bypassing(objects_path: str, commit_hash: str, dict_info: dict)->Non
 def build_tree(commits_info: dict)->str:
     graph = "flowchart TD\n"
     for commit_hash in commits_info:                                            # Bypassing dictionary
-        if commit_hash != "leaf":
-            commit_info = commits_info[commit_hash]
-            graph += f"\t'{commit_hash}'[{commit_info[2]} {commit_info[1]}]\n"  # Calling the vertex
-            for parent in commit_info[0]:                                       # Noting dependencies
-                graph += f"\t'{commit_hash}' --> '{parent}'\n"
+        commit_info = commits_info[commit_hash]
+        graph += f"\t'{commit_hash}'[{commit_info[2]} {commit_info[1]}]\n"      # Calling the vertex
+        for parent in commit_info[0]:                                           # Noting dependencies
+            graph += f"\t'{commit_hash}' --> '{parent}'\n"
     return graph
 
 # Writing Mermaid code to file
@@ -79,11 +78,12 @@ def write_to_file(res_path: str, data: str)->None:
     except FileNotFoundError:
         print(f"{'\033[91m'}It seems that you've entered wrong path to result file.\n\
               \rPlease restart program with correct arguments!{'\033[0m'}")
-        
+
 def main()->None:
     args = get_args()                                                           # Receiving args
     write_to_file(args[2], build_tree(get_commits_info(args[1], args[3])))      # Writing Mermaid code
     os.system(f"python3 {args[0]} {args[2]}")                                   # Launch visualizer script
+
 
 if __name__ == "__main__":
     main()
