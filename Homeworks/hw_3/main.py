8b9f36a233e9be382bda07c79f028e1d950920bd
Ivan <musatov.i.a@edu.mirea.ru>
2024-11-29 18:49:23 +0300

    refactor(main script): add comments to functions and methods

--- Homeworks/hw_3/main.py
+++ Homeworks/hw_3/main.py
@@ -3,11 +3,15 @@ import sys
 from peco import State, parse, program
 
 
+# Implements the process of translating from configuration language to xml format
 class Converter:
+
+    # Checks is it a string value or name of constant
     @staticmethod
     def is_str(s: str) -> bool:
         return re.fullmatch(r"\'[^\']*\'", s) != None
-    
+
+    # Highlights part of string with certain color  
     @staticmethod
     def highligth(s: str, type: str = 'err', beg: int = 0, end: int = -1) -> str:
         lb, rb = '\033[91m', '\033[0m'
@@ -17,33 +21,39 @@ class Converter:
             return s[:beg] + lb + s[beg:len(s)+end+1] + rb
         return s[:beg] + lb + s[beg:end] + rb + s[end:]
 
+    # Standart constructor
     def __init__(self) -> None:
-        self.src: str = ''
-        self.obj: State = None
-        self.constants: dict = dict()
-        self.xml: str = ''
+        self._src: str = ''
+        self._obj: State = None
+        self._constants: dict = dict()
+        self._xml: str = ''
     
+    # Loads the source program written in configuration language
     def load(self, text: str) -> None:
-        self.src = text
+        self._src = text
 
+    # Parses code according to peco rules
     def __parse(self) -> None:
-        self.obj = parse(self.src, program)
+        self._obj = parse(self._src, program)
 
+    # Checks correctness of source code
     def __check_syntax(self) -> bool:
-        if not self.obj.ok:
-            err_ptr = self.obj.glob['err']
-            slice = self.obj.text[:err_ptr+1]
+        if not self._obj.ok:
+            err_ptr = self._obj.glob['err']
+            slice = self._obj.text[:err_ptr+1]
             num = slice.count('\n')
             pos = slice.rfind('\n')
             print(Converter.highligth(f"Синтаксическая ошибка в {num} строке:"))
-            print(Converter.highligth(self.obj.text.split('\n')[num], "err", err_ptr-pos-1, err_ptr-pos))
+            print(Converter.highligth(self._obj.text.split('\n')[num], "err", err_ptr-pos-1, err_ptr-pos))
             return False
         print(Converter.highligth("Синтаксических ошибок не обнаружено", type='ok'))
         return True
 
+    # Appends another string to the xml result 
     def __to_output(self, str: str) -> None:
-        self.xml += str + '\n'
+        self._xml += str + '\n'
 
+    # Serializes code element to xml format
     def __serialize(self, obj: State, name: str = '', indent: str = '', delim: str = '  ') -> None:
         id = ''
         if name != '':
@@ -54,8 +64,8 @@ class Converter:
             if Converter.is_str(obj):
                 self.__to_output(indent + f"<string{id}>{obj[1:-1]}</string>")
             else:
-                if obj in self.constants:
-                    self.__serialize(self.constants[obj], name=name, indent=indent)
+                if obj in self._constants:
+                    self.__serialize(self._constants[obj], name=name, indent=indent)
                 else:
                     print(Converter.highligth(f"There is no constant '{obj}' declared in this scope!"))
                     exit(-1)
@@ -72,33 +82,22 @@ class Converter:
                 self.__to_output(indent + delim + '</entry>')
             self.__to_output(indent + '</dictionary>')
 
+    # Main method that converts source code to xml
     def convert(self) -> str:
         self.__parse()
         if(self.__check_syntax()):
-            stack = self.obj.stack[0]
+            stack = self._obj.stack[0]
             for i in range(len(stack)):
                 if i % 2 == 0:
-                    self.constants[stack[i]] = stack[i+1]
-            for cst in self.constants.items():
+                    self._constants[stack[i]] = stack[i+1]
+            for cst in self._constants.items():
                 self.__serialize(cst[1], name=cst[0])
         else:
             exit(-1)
-        return self.xml[:-1]
+        return self._xml[:-1]
 
-  
-test1 = '''
-    (def name 2)
-    (def new $(name))
-    (def lst [$(new), 2])
-    '''
-test2 = '''
-    // There is a program
-    (def num 5)
-    (def typer [-9, 'plus', 7])
-    ( def dict { ip => [9, 3], mem => 2, sub => {  cnt=>$(num), val => 'true' } } )
-    ( def str 'Hello, World!' )
-    '''
 
+# Reads from standard input
 def read_cmd() -> str:
     input = ''
     for line in sys.stdin:
@@ -107,7 +106,8 @@ def read_cmd() -> str:
 
 def main():
     converter = Converter()
-    print(read_cmd())
+    converter.load(read_cmd())
+    print(converter.convert())
 
 if __name__ == '__main__':
     main()
