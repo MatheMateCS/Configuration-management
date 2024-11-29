e0b6d349b33940871422c9967c62259e143329dd
Ivan <musatov.i.a@edu.mirea.ru>
2024-11-29 14:44:18 +0300

    feat(main script): add syntax error checking and informing

--- Homeworks/hw_3/main.py
+++ Homeworks/hw_3/main.py
@@ -1,6 +1,26 @@
 from peco import *
 
 
+def highligth(s, type='err', beg=0, end=-1) -> str:
+    lb, rb = '\033[91m', '\033[0m'
+    if type == 'ok':
+        lb = '\033[92m'
+    if end < 0:
+        return s[:beg] + lb + s[beg:len(s)+end+1] + rb
+    return s[:beg] + lb + s[beg:end] + rb + s[end:]
+
+def check_syntax(s: State) -> bool:
+    if not s.ok:
+        err_ptr = s.glob['err']
+        slice = s.text[:err_ptr+1]
+        num = slice.count('\n')
+        pos = slice.rfind('\n')
+        print(highligth(f"Синтаксическая ошибка в {num} строке:"))
+        print(highligth(s.text.split('\n')[num], "err", err_ptr-pos-1, err_ptr-pos))
+        return False
+    print(highligth("Синтаксических ошибок не обнаружено", type='ok'))
+    return True
+
 def test():
     src = '''
     (def name 2)
