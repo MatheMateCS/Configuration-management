4d3d1a4066fd8c389f240cb34c24c91d45ec9ee3
Ivan <musatov.i.a@edu.mirea.ru>
2024-11-29 17:15:17 +0300

    feat(main script): add serializing to xml format

--- Homeworks/hw_3/main.py
+++ Homeworks/hw_3/main.py
@@ -21,20 +21,70 @@ def check_syntax(s: State) -> bool:
     print(highligth("Синтаксических ошибок не обнаружено", type='ok'))
     return True
 
-def test():
-    src = '''
+def is_str(s):
+    return re.fullmatch(r"\'[^\']*\'", s) != None
+
+def serialize(obj, name='', indent='', delim='  '):
+    if name != '':
+        name = f" name='{name}'"
+    if type(obj) == float:
+        print(indent + f"<number{name}>{obj}</number>")
+    elif type(obj) == str:
+        if is_str(obj):
+            print(indent + f"<string{name}>{obj[1:-1]}</string>")
+        else:
+            if obj in constants:
+                serialize(constants[obj], indent=indent)
+            else:
+                print(highligth(f"There is no constant '{obj}' declared in this scope!"))
+                exit(0)
+    elif type(obj) == list:
+        print(indent + f"<array{name}>")
+        for el in obj:
+            serialize(el, indent=indent+delim)
+        print(indent + "</array>")
+    elif type(obj) == dict:
+        print(indent + f"<dictionary{name}>")
+        for val in obj.items():
+            print(indent + delim + f"<entry key='{val[0]}'>")
+            serialize(val[1], indent=indent+2*delim)
+            print(indent + delim + '</entry>')
+        print(indent + '</dictionary>')
+    
+
+src = '''
     (def name 2)
     (def new $(name))
     (def lst [$(new), 2])
     '''
-    src2 = '''
+src2 = '''
     // There is a program
+    (def num 5)
+    (def typer [-9, 'plus', 7])
     ( def dict { ip => [9, 3], mem => 2, sub => {  cnt=>$(num), val => 'true' } } )
     ( def str 'Hello, World!' )
     '''
-    s = parse(src2, main)
-    print(s)
-    
+obj = parse(src2, main)
+ss = obj.stack[0]
+constants = dict()
+for i in range(len(ss)):
+    if i % 2 == 0:
+        constants[ss[i]] = ss[i+1]
+print(constants)
+for d in constants.items():
+    serialize(d[1], name=d[0])
+
+def test():
+    pass
+    # for n in s.glob['tab'].values():
+    #     print(n.stack)
+
 test()
+# def main():
+#     ...
+
+# if __name__ == '__main__':
+#     test()
+    
 
 
